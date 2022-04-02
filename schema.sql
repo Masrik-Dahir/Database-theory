c011bd47cf059399bc440d3e85a1c84cdc48258f
nguyenvt35 <69822014+nguyenvt35@users.noreply.github.com>
2022-04-02 15:01:27 -0400

    Create scheme.sql
    SQL code for the table schema under project_nguyenvt35

---
+++ schema.sql
@@ -0,0 +1,223 @@
+CREATE TABLE User (
+    TIN NUMERIC(9,0),
+    first_name VARCHAR(20) NOT NULL,
+    last_name VARCHAR(20) NOT NULL,
+    email_address VARCHAR(40) NOT NULL UNIQUE,
+    street_address VARCHAR(20) NOT NULL,
+    city VARCHAR(20) NOT NULL,
+    county VARCHAR(20) NOT NULL,
+    state_abbr CHAR(2),
+    zip_code NUMERIC(5, 0),
+    date_of_birth DATE NOT NULL,
+    user_type CHAR(2) NOT NULL,
+    PRIMARY KEY (TIN)
+)
+
+CREATE TABLE User_Phone (
+    TIN NUMERIC(9,0),
+    phone_number NUMERIC(10,0) UNIQUE,
+    PRIMARY KEY (TIN, phone_number),
+    FOREIGN KEY (TIN) REFERENCES User(TIN)
+)
+
+CREATE TABLE User_Bank_Account (
+    TIN NUMERIC(9,0),
+    Bank_Account_Number NUMERIC(12,0),
+    Routing_Number NUMERIC(9,0),
+    PRIMARY KEY (TIN, bank_account_number, routing_number),
+    FOREIGN KEY (TIN) REFERENCES User(TIN)
+)
+
+CREATE TABLE User_Transaction (
+    TIN NUMERIC(9,0),
+    Transaction_Number NUMERIC(12, 0),
+    asset_type VARCHAR(15) NOT NULL,
+    PRIMARY KEY (TIN, Transaction_Number),
+    FOREIGN KEY (TIN) REFERENCES User(TIN)
+)
+
+CREATE TABLE Brokerage (
+    EIN NUMERIC(9,0),
+    commission_pct FLOAT,
+    license VARCHAR(20),
+    leverage_trading BIT NOT NULL,
+    PRIMARY KEY (EIN),
+    FOREIGN KEY (EIN) REFERENCES User(TIN)
+)
+
+CREATE TABLE Brokerage_Account (
+    SSN NUMERIC(9,0),
+    TIN NUMERIC(9,0),
+    Account_Number NUMERIC(12,0),
+    Account_Routing_Number NUMERIC(9,0),
+    PRIMARY KEY (SSN, TIN, Brokerage_Account_Number, Brokerage_Account_Routing_Number)
+    FOREIGN KEY (SSN) REFERENCES Individual_Investor(SSN),
+    FOREIGN KEY (TIN) REFERENCES Brokerage(EIN)
+)
+
+CREATE TABLE Bank_Relation (
+    Brokerage_Account_Number NUMERIC(12,0),
+    Brokerage_Account_Routing_Number NUMERIC (9,0),
+    II_Bank_Account_Number NUMERIC(12,0),
+    II_Bank_Account_Routing_Number NUMERIC(9,0),
+    PRIMARY KEY (Brokerage_Account_Number, Brokerage_Account_Routing_Number, II_Bank_Account_Number, II_Bank_Account_Routing_Number),
+    FOREIGN KEY (Brokerage_Account_Number, Brokerage_Account_Routing_Number) REFERENCES Brokerage_Account (Account_Number, Account_Routing_Number)
+)
+
+CREATE TABLE Individual_Investor (
+    SSN NUMERIC(9,0),
+    first_name VARCHAR(20) NOT NULL,
+    last_name VARCHAR(20) NOT NULL,
+    email_address VARCHAR(40) NOT NULL UNIQUE,
+    street_address VARCHAR(20) NOT NULL,
+    city VARCHAR(20) NOT NULL,
+    county VARCHAR(20) NOT NULL,
+    state_abbr CHAR(2) NOT NULL,
+    zip_code NUMERIC(5, 0),
+    date_of_birth DATE NOT NULL,
+    PRIMARY KEY (SSN)
+)
+
+CREATE TABLE Individual_Investor_Phone (
+    SSN NUMERIC(9,0),
+    phone_number NUMERIC(10,0) UNIQUE,
+    PRIMARY KEY (SSN, phone_number)
+)
+
+CREATE TABLE Direct_Investor (
+    TIN NUMERIC(9,0),
+    public_trading BIT NOT NULL,
+    PRIMARY KEY (TIN),
+    FOREIGN KEY (TIN) REFERENCES User(TIN)
+)
+
+CREATE TABLE Exchange (
+    Market_Identifier_Code CHAR(4),
+    CEO VARCHAR(40),
+    currency VARCHAR(10) NOT NULL,
+    number_of_securities INT NOT NULL,
+    market_cap INT NOT NULL,
+    website VARCHAR(40),
+    date_founded DATE NOT NULL,
+    PRIMARY KEY (Market_Identifier_Code)
+)
+
+CREATE TABLE Exchange_Index (
+    Market_Identifier_Code CHAR(4),
+    Market_Index VARCHAR(10),
+    PRIMARY KEY (Market_Identifier_Code, Market_Index),
+    FOREIGN KEY (Market_Identifier_Code) REFERENCES Exchange(Market_Identifier_Code)
+)
+
+CREATE TABLE ETF (
+    ETF_Symbol VARCHAR(5),
+    dividend FLOAT NOT NULL,
+    company VARCHAR(20) NOT NULL,
+    unit_price FLOAT NOT NULL,
+    etf_name VARCHAR(30) NOT NULL,
+    number_of_shares INT NOT NULL,
+    PRIMARY KEY (ETF_Symbol)
+)
+
+CREATE TABLE ETF_Transaction (
+    TIN NUMERIC(9,0),
+    Transaction_Number NUMERIC(12,0),
+    ETF_Symbol VARCHAR(5) NOT NULL,
+    quantity INT NOT NULL,
+    PRIMARY KEY (TIN, Transaction_Number),
+    FOREIGN KEY (TIN, Transaction_Number) REFERENCES User_Transaction(TIN, Transaction_Number),
+    FOREIGN KEY (ETF_Symbol) REFERENCES ETF(ETF_Symbol)
+)
+
+CREATE TABLE Exchange_ETFs (
+    Market_Identifier_Code VARCHAR(4),
+    ETF_Symbol VARCHAR(5),
+    PRIMARY KEY (Market_Identifier_Code, ETF_Symbol),
+    FOREIGN KEY (Market_Identifier_Code) REFERENCES Exchange(Market_Identifier_Code),
+    FOREIGN KEY (ETF_Symbol) REFERENCES ETF(ETF_Symbol)
+)
+
+CREATE TABLE Cryptocurrency (
+    Cryptocurrency_Symbol VARCHAR(5),
+    total_supply INT NOT NULL
+    current_supply INT NOT NULL,
+    unit_price FLOAT NOT NULL,
+    cryptocurrency_name VARCHAR(20) NOT NULL,
+    PRIMARY KEY (Cryptocurrency_Symbol)
+)
+
+CREATE TABLE Cryptocurrency_Transaction (
+    TIN NUMERIC(9,0),
+    Transaction_Number NUMERIC(12,0),
+    Cryptocurrency_Symbol VARCHAR(5) NOT NULL,
+    quantity INT NOT NULL,
+    PRIMARY KEY (TIN, Transaction_Number),
+    FOREIGN KEY (TIN, Transaction_Number) REFERENCES User_Transaction(TIN, Transaction_Number),
+    FOREIGN KEY (Cryptocurrency_Symbol) REFERENCES Cryptocurrency(Cryptocurrency_Symbol)
+)
+
+CREATE TABLE Exchange_Cryptocurrencies (
+    Market_Identifier_Code VARCHAR(4),
+    Cryptocurrency_Symbol VARCHAR(5),
+    PRIMARY KEY (Market_Identifier_Code, Cryptocurrency_Symbol),
+    FOREIGN KEY (Market_Identifier_Code) REFERENCES Exchange(Market_Identifier_Code),
+    FOREIGN KEY (Cryptocurrency_Symbol) REFERENCES Cryptocurrency(Cryptocurrency_Symbol)
+)
+
+CREATE TABLE Mutual_Fund (
+    Mutual_Fund_Symbol VARCHAR(5),
+    dividend FLOAT NOT NULL,
+    unit_price FLOAT NOT NULL,
+    asset_class VARCHAR(10) NOT NULL,
+    fund_name VARCHAR(20) NOT NULL,
+    net_asset_value FLOAT NOT NULL,
+    PRIMARY KEY (Mutual_Fund_Symbol)
+)
+
+CREATE TABLE Mutual_Fund_Transaction (
+    TIN NUMERIC(9,0),
+    Transaction_Number NUMERIC(12,0),
+    Mutual_Fund_Symbol VARCHAR(5) NOT NULL,
+    quantity INT NOT NULL,
+    PRIMARY KEY (TIN, Transaction_Number),
+    FOREIGN KEY (TIN, Transaction_Number) REFERENCES User_Transaction(TIN, Transaction_Number),
+    FOREIGN KEY (Mutual_Fund_Symbol) REFERENCES Mutual_Fund(Mutual_Fund_Symbol)
+)
+
+CREATE TABLE Exchange_Mutual_Funds (
+    Market_Identifier_Code VARCHAR(4),
+    Mutual_Fund_Symbol VARCHAR(5),
+    PRIMARY KEY (Market_Identifier_Code, Mutual_Fund_Symbol),
+    FOREIGN KEY (Market_Identifier_Code) REFERENCES Exchange(Market_Identifier_Code),
+    FOREIGN KEY (Mutual_Fund_Symbol) REFERENCES Mutual_Fund(Mutual_Fund_Symbol)
+)
+
+
+CREATE TABLE Options (
+    Option_Symbol VARCHAR(5),
+    expiration_date DATE NOT NULL,
+    strike_price FLOAT NOT NULL,
+    asset_class VARCHAR(10) NOT NULL,
+    company_name VARCHAR(20) NOT NULL,
+    PRIMARY KEY (Option_Symbol)
+)
+
+CREATE TABLE Options_Transaction (
+    TIN NUMERIC(9,0),
+    Transaction_Number NUMERIC(12,0),
+    Option_Symbol VARCHAR(5) NOT NULL,
+    quantity INT NOT NULL,
+    PRIMARY KEY (TIN, Transaction_Number),
+    FOREIGN KEY (TIN, Transaction_Number) REFERENCES User_Transaction(TIN, Transaction_Number),
+    FOREIGN KEY (Option_Symbol) REFERENCES Options(Option_Symbol)
+)
+
+CREATE TABLE Exchange_Options (
+    Market_Identifier_Code VARCHAR(4),
+    Option_Symbol VARCHAR(5),
+    PRIMARY KEY (Market_Identifier_Code, Option_Symbol),
+    FOREIGN KEY (Market_Identifier_Code) REFERENCES Exchange(Market_Identifier_Code),
+    FOREIGN KEY (Option_Symbol) REFERENCES Options(Option_Symbol)
+
+
+
