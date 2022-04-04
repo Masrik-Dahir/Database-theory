37624ae5605d67bb3dc608a56bba2e1941cdec32
nguyenvt35 <69822014+nguyenvt35@users.noreply.github.com>
2022-04-03 22:32:45 -0400

    Update instance.sql
    Corrected commas

--- instance.sql
+++ instance.sql
@@ -1,9 +1,9 @@
-INSERT INTO Users VALUES --emails and dates are all bogus
-(941737782, "Charles Schwab", "emailNotFound@email.com", "211 Main St", "San Fransisco", "CA", 94105, '1970-01-01', "BK"),
-(821664890, "Webull Financial Llc", "webull@bogusemail.com", "44 Wall St, Suite 501", "New York", "NY", 10005, '2010-11-12', "BK"),
-(942404110, "Apple Inc.", "apple@appleemail.com", "One Apple Park Way", "Cupertino", "CA", 95014, '1960-12-25', "DI"),
-(911144442, "Microsoft Corp.", "microsoft@hotmail.com", "One Microsoft Way", "Redmond", "WA", 98052, '1980-02-15', "DI"),
-(133963293, "JP Morgan Fleming Mutual Fund Group Inc.", "jpmorgan@email.com", "277 Park Avenue", "New York", "NY", 10172, '1820-06-30', "BK")
+INSERT INTO Users VALUES -- emails and dates are all bogus
+(941737782, 'Charles Schwab', 'emailNotFound@email.com', '211 Main St', 'San Fransisco', 'CA', 94105, '1970-01-01', 'BK'),
+(821664890, 'Webull Financial Llc', 'webull@bogusemail.com', '44 Wall St, Suite 501', 'New York', 'NY', 10005, '2010-11-12', 'BK'),
+(942404110, 'Apple Inc.', 'apple@appleemail.com', 'One Apple Park Way', 'Cupertino', 'CA', 95014, '1960-12-25', 'DI'),
+(911144442, 'Microsoft Corp.', 'microsoft@hotmail.com', 'One Microsoft Way', 'Redmond', 'WA', 98052, '1980-02-15', 'DI'),
+(133963293, 'JP Morgan Fleming Mutual Fund Group Inc.', 'jpmorgan@email.com', '277 Park Avenue', 'New York', 'NY', 10172, '1820-06-30', 'BK')
 ;
 
 INSERT INTO Users_Phone VALUES 
@@ -15,7 +15,7 @@ INSERT INTO Users_Phone VALUES
 (133963293, 8004804111)
 ;
 
---all values here are made up
+-- all values here are made up
 INSERT INTO Users_Bank_Account VALUES 
 (941737782, 996318325214, 442136658),
 (821664890, 614324902932, 875919996),
@@ -26,30 +26,30 @@ INSERT INTO Users_Bank_Account VALUES
 ;
 
 INSERT INTO Users_Transaction VALUES 
-(941737782, 595283550234, "Mutual Fund"),
-(941737782, 292743994121, "ETF"),
-(133963293, 427330228709, "ETF"),
-(133963293, 216811147728, "Option"),
-(133963293, 215796003877, "Mutual Fund")
-(821664890, 390629840269, "Cryptocurrency"),
-(821664890, 499737848193, "Cryptocurrency"),
-(942404110, 671238532132, "ETF"),
-(941737782, 851701468945, "ETF"),
-(942404110, 553958317259, "Option")
+(941737782, 595283550234, 'Mutual Fund'),
+(941737782, 292743994121, 'ETF'),
+(133963293, 427330228709, 'ETF'),
+(133963293, 216811147728, 'Option'),
+(133963293, 215796003877, 'Mutual Fund'),
+(821664890, 390629840269, 'Cryptocurrency'),
+(821664890, 499737848193, 'Cryptocurrency'),
+(942404110, 671238532132, 'ETF'),
+(941737782, 851701468945, 'ETF'),
+(942404110, 553958317259, 'Option')
 ;
 
 
---All people are completely made up
+-- All people are completely made up
 INSERT INTO Individual_Investor VALUES 
-(123456789, "John", "Doe", "jdoe@email.com", "123 Nowhere St", "Nunya", "NO", 12345, '1970-01-01'),
-(987654321, "Jane", "Doe", "jdoe1@email.com", "321 Somewhere St", "London", "TX", 37218, '1970-12-25'),
-(777777777, "Bob", "Bobbington", "bbobington@email.com", "9841 4th St", "New York City", "NY", 10001, '1963-11-21'),
-(963852741, "Louise", "Carting", "lcarting@email.com", "30058 Rudiger Rd", "Rome", "GA", 30149, '1992-08-15'),
-(666666666, "Hilda", "Hilda", "hhilda@email.com", "22 Hilda Blvd", "Hildatown", "OH", 77379, '1966-06-16')
+(123456789, 'John', 'Doe', 'jdoe@email.com', '123 Nowhere St', 'Nunya', 'NO', 12345, '1970-01-01'),
+(987654321, 'Jane', 'Doe', 'jdoe1@email.com', '321 Somewhere St', 'London', 'TX', 37218, '1970-12-25'),
+(777777777, 'Bob', 'Bobbington', 'bbobington@email.com', '9841 4th St', 'New York City', 'NY', 10001, '1963-11-21'),
+(963852741, 'Louise', 'Carting', 'lcarting@email.com', '30058 Rudiger Rd', 'Rome', 'GA', 30149, '1992-08-15'),
+(666666666, 'Hilda', 'Hilda', 'hhilda@email.com', '22 Hilda Blvd', 'Hildatown', 'OH', 77379, '1966-06-16')
 ;
 
 
---Imaginary people's phone numbers are also imaginary
+-- Imaginary people's phone numbers are also imaginary
 INSERT INTO Individual_Investor_Phone VALUES 
 (123456789, 2223334444),
 (123456789, 8011111111),
@@ -57,25 +57,25 @@ INSERT INTO Individual_Investor_Phone VALUES
 (963852741, 1472583690)
 ;
 
---License values for brokerages are made up
+-- License values for brokerages are made up
 INSERT INTO Brokerage VALUES 
-(941737782, 25.00, "Some License", 1),
-(821664890, 55.00, "Some License", 0),
-(133963293, 25.00, "Some License", 1);
+(941737782, 25.00, 'Some License', 1),
+(821664890, 55.00, 'Some License', 0),
+(133963293, 25.00, 'Some License', 1);
 
---Brokerage accounts for imaginary people are also imaginary
+-- Brokerage accounts for imaginary people are also imaginary
 INSERT INTO Brokerage_Account VALUES 
-(941737782, 123456789, 151603913001, 607387354),
-(821664890, 123456789, 151603913001, 607387354),
-(133963293, 123456789, 871400138715, 282374573),
-(821664890, 987654321, 955388404377, 262822116),
-(941737782, 777777777, 788493666846, 474327555),
-(133963293, 777777777, 788493666846, 474327555),
-(133963293, 963852741, 425256348988, 222494623),
-(941737782, 666666666, 897829058341, 909375924)
+(123456789, 941737782, 151603913001, 607387354),
+(123456789, 821664890, 151603913001, 607387354),
+(123456789, 133963293, 871400138715, 282374573),
+(987654321, 821664890, 955388404377, 262822116),
+(777777777, 941737782, 788493666846, 474327555),
+(777777777, 133963293, 788493666846, 474327555),
+(963852741, 133963293, 425256348988, 222494623),
+(666666666, 941737782, 897829058341, 909375924)
 ;
 
---As are their bank relations
+-- As are their bank relations
 INSERT INTO Bank_Relation VALUES 
 (151603913001, 607387354, 672705921241, 615113802),
 (871400138715, 282374573, 401017261321, 437773922),
@@ -88,18 +88,18 @@ INSERT INTO Direct_Investor VALUES
 (942404110, 1),
 (911144442, 1);
 
---number of securities is simply an approximation, market cap in billions
+-- number of securities is simply an approximation, market cap in billions
 INSERT INTO Exchange (Market_Identifier_Code, currency, number_of_securities, market_cap, date_founded) VALUES 
-("NYSE", "US Dollars", 2800, 1848, '1792-05-17'),
-("NASDAQ", "US Dollars", 3300, 2818, '1971-02-08'),
-("CRYPTO.COM", "US Dollars", 192, 2)
-("BINANCE", "US Dollars", 395, 19);
+('NYSE', 'US Dollars', 2800, 1848, '1792-05-17'),
+('NASDAQ', 'US Dollars', 3300, 2818, '1971-02-08'),
+('CRYPTO.COM', 'US Dollars', 192, 2, '2016-01-01'),
+('BINANCE', 'US Dollars', 395, 19, '2017-01-01');
 
 INSERT INTO Exchange_Index VALUES 
-("NYSE", "NYA"),
-("NASDAQ", "COMP");
+('NYSE', 'NYA'),
+('NASDAQ', 'COMP');
 
---stocks numbered by thousands, market cap numbered in millions
+-- stocks numbered by thousands, market cap numbered in millions
 INSERT INTO ETF VALUES 
 ('CCL', 0.50, 'Carnival Corporation & plc', 20.05, 981048, 23373),
 ('AAPL', 0.2200, 'Apple Inc.', 174.31, 16406400, 2859799),
@@ -123,10 +123,10 @@ INSERT INTO ETF VALUES
 ;
 
 INSERT INTO ETF_Transaction VALUES 
-(941737782, 292743994121, "TSLA", 100),
-(133963293, 427330228709, "AMZN", 150),
-(942404110, 671238532132, "GOOG", 300);
-(941737782, 851701468945, "SM", 80);
+(941737782, 292743994121, 'TSLA', 100),
+(133963293, 427330228709, 'AMZN', 150),
+(942404110, 671238532132, 'GOOG', 300),
+(941737782, 851701468945, 'SM', 80);
 
 INSERT INTO Exchange_ETFs VALUES 
 ('NASDAQ', 'CCL'),
@@ -159,40 +159,40 @@ INSERT INTO Cryptocurrency VALUES
 ;
 
 INSERT INTO Cryptocurrency_Transaction VALUES 
-(821664890, 390629840269, "BTC-USD", 50),
-(821664890, 499737848193, "ETH-USD", 100);
+(821664890, 390629840269, 'BTC-USD', 50),
+(821664890, 499737848193, 'ETH-USD', 100);
 
 INSERT INTO Exchange_Cryptocurrencies VALUES 
-("CRYPTO.COM", 'BTC-USD'),
-("BINANCE", 'BTC-USD'),
-("CRYPTO.COM", 'ETH-USD'),
-("BINANCE", 'ETH-USD'),
-("CRYPTO.COM", 'ADA-USD'),
-("BINANCE", 'ADA-USD'),
-("CRYPTO.COM", 'SOL-USD'),
-("BINANCE", 'SOL-USD'),
-("CRYPTO.COM", 'ALGO-USD'),
-("BINANCE", 'ALGO-USD'),
-("CRYPTO.COM", 'LUNA-USD'),
-("BINANCE", 'LUNA-USD')
+('CRYPTO.COM', 'BTC-USD'),
+('BINANCE', 'BTC-USD'),
+('CRYPTO.COM', 'ETH-USD'),
+('BINANCE', 'ETH-USD'),
+('CRYPTO.COM', 'ADA-USD'),
+('BINANCE', 'ADA-USD'),
+('CRYPTO.COM', 'SOL-USD'),
+('BINANCE', 'SOL-USD'),
+('CRYPTO.COM', 'ALGO-USD'),
+('BINANCE', 'ALGO-USD'),
+('CRYPTO.COM', 'LUNA-USD'),
+('BINANCE', 'LUNA-USD')
 ;
 
---Net Asset Value in Millions
+-- Net Asset Value in Millions
 INSERT INTO Mutual_Fund VALUES 
 ('VSMPX', 0.652, 208.71, 'stocks', 'Vanguard Total Stock Market Index Fund Institu...', 1767408),
 ('SPY', 1.366, 452.92, 'stocks', 'SPDR S&P 500 ETF Trust', 415681),
-('VTSAX', 0.342, 111.28, 'stocks', 'Vanguard Total Stock Market Index Fund Admiral...', 942346)
+('VTSAX', 0.342, 111.28, 'stocks', 'Vanguard Total Stock Market Index Fund Admiral...', 942346),
 ('PLD', 0.79, 165.8, 'real estate', 'Prologis, Inc.', 122722),
 ('GCCIX', 1.488, 11.49, 'real estate', 'Goldman Sachs Commodity Strategy Fund Institut...', 100)
 ;
 
 INSERT INTO Mutual_Fund_Transaction VALUES 
-(941737782, 595283550234, 'SPY', 500)
+(941737782, 595283550234, 'SPY', 500),
 (133963293, 215796003877, 'GCCIX', 126) 
 ;
 
 INSERT INTO Exchange_Mutual_Funds VALUES 
-('NASDAQ', 'VSMPX')
+('NASDAQ', 'VSMPX'),
 ('NYSE', 'SPY'),
 ('NASDAQ', 'SPY'),
 ('NASDAQ', 'VTSAX'),
